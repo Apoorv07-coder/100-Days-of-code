cd483c2dca4715b367721a15ed3b5bb0dbce32f7
Apoorv07-coder <apoorvmishra2007@gmail.com>
2025-08-26 19:40:50 +0530

    Day5.1.c

--- Day5.1.c
+++ Day5.1.c
@@ -1,3 +1,18 @@
+//Q9: Write a program to calculate simple and compound interest for given principal, rate, and time.
+
+/*
+Sample Test Cases:
+Input 1:
+1000 5 2
+Output 1:
+Simple Interest=100, Compound Interest=102.5
+
+Input 2:
+5000 7 3
+Output 2:
+Simple Interest=1050, Compound Interest=1125.76
+
+*/
 #include <stdio.h>
 
 int main() {
@@ -19,4 +34,5 @@ int main() {
     printf("Compound Interest = %.2f\n", compoundInterest);
 
     return 0;
+
 }

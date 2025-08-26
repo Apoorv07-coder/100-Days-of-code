dd51d74a9ec9055c2793092649877a1d0e274358
Apoorv07-coder <apoorvmishra2007@gmail.com>
2025-08-26 18:48:15 +0530

    Day3.1.c

--- Day3.1.c
+++ Day3.1.c
@@ -1,3 +1,18 @@
+//Q5: Write a program to convert temperature from Celsius to Fahrenheit.
+
+/*
+Sample Test Cases:
+Input 1:
+0
+Output 1:
+Fahrenheit=32
+
+Input 2:
+100
+Output 2:
+Fahrenheit=212
+
+*/
 #include<stdio.h>
 int main(){
     int C;
@@ -6,4 +21,5 @@ int main(){
     int  F = (C * 9/5) + 32;
     printf("Hence, the temperature in Farenheit is : %d",F);
     return 0;
+
 }
